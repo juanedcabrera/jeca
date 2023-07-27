f7722bdccbdc103273998548c146c197c9baab18
juanedcabrera <juanedcabrera@gmail.com>
2023-07-27 14:02:48 -0600

    Added Vite React App

--- jeca_backend/views.py
+++ jeca_backend/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def home_view(request):
+    return JsonResponse({"message": "Welcome to the home page!"})
+
+def jobs_view(request):
+    return JsonResponse({"mesage": "This is the jobs page!"})
+
+def docs_view(request):
+    return JsonResponse({'message': 'This is the page for all documents'})
+
+def company_view(request):
+    return JsonResponse({'message': 'This is the page with all the companies'})
+
+def docs_view(request):
+    return JsonResponse({'message': 'This is the page for all documents'})
